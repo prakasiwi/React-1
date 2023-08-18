a15ea57847d704d5ec03422eea04e6b2d2981866
prakasiwi <muhammadgalih282@gmail.com>
2023-08-18 23:15:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+//import { apiKeys } from './util'; //untuk import 1 variable dalam 1 file
+//import apiKeys2 from './util'; //import default tidak perlu bracket
+import * as util from './util'; //import untuk group export
 
 function App() {
+  //console.log(apiKeys);
+  //console.log(apiKeys2);
+  console.log(util.apiKeys3); //cara cek berhasil import dengan group
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/util.js
@@ -0,0 +1,3 @@
+export let apiKeys = "ContohAPI1"; //penerapan untuk export
+export default "defaultExp"; //default tidak perlu nama variable dan 1 file tidak dapat berisi 2 default
+export let apiKeys3 = "API ke 3";
